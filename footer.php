32bbca6c0927a577331dfa08771b0370bb1d3332
Davide <you@example.com>
2018-06-03 16:50:38 +0200

    finito es

--- footer.php
+++ footer.php
@@ -1,5 +1,14 @@
    <footer>
       <?php wp_footer(); ?>
+
+      <div id="footer-sidebar">
+         <?php
+            if(is_active_sidebar('footer_sidebar')){
+               dynamic_sidebar('footer_sidebar');
+            }
+         ?>
+      </div>
+
    </footer>
    </body>
 </html>
--- functions.php
+++ functions.php
@@ -1,4 +1,5 @@
 <?php
+//initialize nav menù
    if(!function_exists('mytheme_setup')){
       function mytheme_setup(){
          register_nav_menus(
@@ -8,6 +9,7 @@
       add_action('after_setup_theme', 'mytheme_setup');
    }
 
+   //import style.css
    if(!function_exists('import_style')){
       function import_style(){
          wp_enqueue_style('my-style', get_template_directory_uri() . '/style.css');
@@ -22,5 +24,17 @@
          wp_enqueue_style('nav-font', 'https://fonts.googleapis.com/css?family=Oxygen+Mono');
       }
       add_action('wp_enqueue_scripts', 'import_fonts');
+   }
+
+   //initialize footer sidebar
+   if(!function_exists('footer_sidebar')){
+      function footer_sidebar(){
+         register_sidebar(array(
+            'name'          => __('Footer sidebar'),
+            'id'            => 'footer_sidebar',
+            'class'         => 'footer_cnt'
+         ));
+      }
+      add_action('widgets_init', 'footer_sidebar');
    }
 ?>
